bf72e19e2fe88c0aaf9ab3514593e9b051a3d6c7
AE52 <eren.sistemmep@gmail.com>
2024-04-09 03:24:14 +0300

    yeniguncelleme

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,28 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'FNC Yapı',
+    description: `Fnc yapı`,
+    imgSrc: '',
+    href: '/blog/fncyapi',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Duyar Hukuk ve Danışmanlık',
+    description: `Duyar hukuk`,
+    imgSrc: '',
+    href: '/blog/duyarhukuk',
+  },
+  {
+    title: 'DMR Karbon Güçlendirme',
+    description: `DMR Karbon Güçlendirme`,
+    imgSrc: '',
+    href: '/blog/dmrkarbonguclendirme',
   },
 ]
 
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,8 +2,8 @@
 const siteMetadata = {
   title: 'Blog',
   author: 'AE52',
-  headerTitle: 'AEblog',
-  description: 'burası ae52 blog sayfası',
+  headerTitle: 'AE blog',
+  description: 'Projeler ve bilgilerimiz',
   language: 'tr-TR',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
